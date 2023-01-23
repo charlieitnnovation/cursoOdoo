dd196c6f4d9df7bfba868e731923f4162cf06c64
= <=>
2023-01-23 20:24:23 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+
+    'summary': 'Academy app to manage Training',
+
+    'description': """
+        Academy modules:
+        - Courses
+        - Sessions
+        - Atendees
+    """,
+
+    'author': 'Itnnovation',
+    
+    'website': '',
+    
+    'category': 'Training',
+    
+    'version'; '16.0',
+    
+    'depends': ['base'],
+    
+    'data': [],
+
+    'demo': [],
+}
